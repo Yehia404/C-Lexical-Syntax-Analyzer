5bbccde1c9d68a62ebe7be7328d15a461391415e
Yehia Sakr <yehiasakr4676@gmail.com>
2024-03-15 17:24:34 +0200

    modify tokenizeLine function

--- Lexical_Analyzer.java
+++ Lexical_Analyzer.java
@@ -4,31 +4,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.*;
 
 
 public class Lexical_Analyzer {
-    private static final List<String> KEYWORDS = Arrays.asList(
-            "auto", "break", "case", "char", "const", "continue", "default", "do",
-            "double", "else", "enum", "extern", "float", "for", "goto", "if",
-            "int", "long", "register", "return", "short", "signed", "sizeof", "static",
-            "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-    );
-
-    private static final List<String> OPERATORS = Arrays.asList(
-            "+", "-", "*", "/", "%", "=", "==", "!=", "<", ">", "<=", ">=", "&&", "||",
-            "!", "&", "|", "^", "~", "<<", ">>", "++", "--", "+=", "-=", "*=", "/=", "%=",
-            "&=", "|=", "^=", "<<=", ">>="
-    );
-
-    private static final List<Character> SPECIAL_SYMBOLS = Arrays.asList(
-            '(', ')', '{', '}', '[', ']', ';', ',', '.'
-    );
-
-    private static final List<String> TYPES = Arrays.asList(
-            "int", "char", "float", "double", "void"
-    );
+    private static final String KEYWORD_PATTERN = "\\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b";
+    private static final String OPERATOR_PATTERN = "[-+*/%<>=!&|~^]+";
+    private static final String IDENTIFIER_PATTERN = "[a-zA-Z_]\\w*";
+    private static final String NUMBER_PATTERN = "\\d+";
+    private static final String STRING_PATTERN = "\"([^\"]*)\"";
+    private static final String SYMBOL_PATTERN = "[(){}\\[\\];,\\.]";
     public static void main(String[] args){
-        String sourceCodeFile = " ";
+        String sourceCodeFile = "E:\\ASU\\Semester 6\\Compilers\\Project\\test.c";
         List <Token> tokens = tokenizeSourceCode(sourceCodeFile);
         for (Token token : tokens) {
             System.out.println(token);
@@ -51,8 +38,42 @@ public class Lexical_Analyzer {
     return tokens;
     }
 
-    private static List<Token> tokenizeLine(String Line,int lineNumber){
+    private static List<Token> tokenizeLine(String line,int lineNumber){
         List<Token> tokens = new ArrayList<>();
-//        yehia
+
+        Pattern pattern = Pattern.compile(
+                KEYWORD_PATTERN + "|" +
+                        OPERATOR_PATTERN + "|" +
+                        IDENTIFIER_PATTERN + "|" +
+                        NUMBER_PATTERN + "|" +
+                        STRING_PATTERN + "|" +
+                        SYMBOL_PATTERN
+        );
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+            String tokenValue = matcher.group();
+            String tokenType;
+
+            if (tokenValue.matches(KEYWORD_PATTERN)) {
+                tokenType = "KEYWORD";
+            } else if (tokenValue.matches(OPERATOR_PATTERN)) {
+                tokenType = "OPERATOR";
+            } else if (tokenValue.matches(IDENTIFIER_PATTERN)) {
+                tokenType = "IDENTIFIER";
+            } else if (tokenValue.matches(NUMBER_PATTERN)) {
+                tokenType = "NUMBER";
+            } else if (tokenValue.matches(STRING_PATTERN)) {
+                tokenType = "STRING";
+            } else if (tokenValue.matches(SYMBOL_PATTERN)) {
+                tokenType = "SYMBOL";
+            } else {
+                tokenType = "UNKNOWN";
+            }
+
+            tokens.add(new Token(tokenType,lineNumber,tokenValue));
+        }
+
+        return tokens;
     }
 }
--- Token.java
+++ Token.java
@@ -1,13 +1,13 @@
 public class Token {
-    public enum SymbolType {
-        IDENTIFIER,STRING,INTEGER,FLOAT,PLUS,MINUS,TIMES,DIVIDE,KEYWORD,INVALID,
-        ASSIGNMENT_OPERATOR,SEMICOLON, LEFT_PARENTHESIS, RIGHT_PARENTHESIS,COMMA,DOT,POINTER_DOT,COLON,EQUAL, LOWER_OR_EQUALS, GREATER_OR_EQUALS, NOT_EQUALS, GREATER_THAN, LOWER_THAN, AT_SIGN,
-        LEFT_BRACE, RIGHT_BRACE
-    }
-    private SymbolType type;
+//    public enum SymbolType {
+//        IDENTIFIER,STRING,INTEGER,FLOAT,PLUS,MINUS,TIMES,DIVIDE,KEYWORD,INVALID,
+//        ASSIGNMENT_OPERATOR,SEMICOLON, LEFT_PARENTHESIS, RIGHT_PARENTHESIS,COMMA,DOT,POINTER_DOT,COLON,EQUAL, LOWER_OR_EQUALS, GREATER_OR_EQUALS, NOT_EQUALS, GREATER_THAN, LOWER_THAN, AT_SIGN,
+//        LEFT_BRACE, RIGHT_BRACE
+//    }
+    private String type;
     private int lineNumber;
-    private String value;
-    public Token(SymbolType t, int lineNumber, String val) {
+    private java.lang.String value;
+    public Token(String t, int lineNumber, java.lang.String val) {
         type = t;
         this.lineNumber = lineNumber;
         value = val;
@@ -15,7 +15,7 @@ public class Token {
 
 
 
-    public SymbolType getType() {
+    public String getType() {
         return type;
     }
 
@@ -23,17 +23,25 @@ public class Token {
         return lineNumber;
     }
 
-    public String getValue() {
+    public java.lang.String getValue() {
         return value;
     }
 
     public void setLineNumber(int lineNumber) {
         this.lineNumber = lineNumber;
     }
-    public void setType(SymbolType type) {
+    public void setType(String type) {
         this.type = type;
     }
-    public void setValue(String value) {
+    public void setValue(java.lang.String value) {
         this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "Token{" +
+                "type=" + type +
+                ", value='" + value + '\'' +
+                ", lineNumber=" + lineNumber +
+                '}';
     }
 }
