64b018fab3fbb6cb6d0b90648636ea12859db4cc
abohoss <21p0141@eng.asu.edu.eg>
2024-03-15 17:35:59 +0200

    chnages

--- Lexical_Analyzer.java
+++ Lexical_Analyzer.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lexical_Analyzer {
     private static final String KEYWORD_PATTERN = "\\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b";
@@ -14,8 +15,10 @@ public class Lexical_Analyzer {
     private static final String NUMBER_PATTERN = "\\d+";
     private static final String STRING_PATTERN = "\"([^\"]*)\"";
     private static final String SYMBOL_PATTERN = "[(){}\\[\\];,\\.]";
+
+    private static final String  PRE_PROCESSOR_PATTERN = "#\\s*\\w+";
     public static void main(String[] args){
-        String sourceCodeFile = "E:\\ASU\\Semester 6\\Compilers\\Project\\test.c";
+        String sourceCodeFile = "test.c";
         List <Token> tokens = tokenizeSourceCode(sourceCodeFile);
         for (Token token : tokens) {
             System.out.println(token);
@@ -47,15 +50,18 @@ public class Lexical_Analyzer {
                         IDENTIFIER_PATTERN + "|" +
                         NUMBER_PATTERN + "|" +
                         STRING_PATTERN + "|" +
-                        SYMBOL_PATTERN
+                        SYMBOL_PATTERN + "|" +
+                        PRE_PROCESSOR_PATTERN
+
         );
         Matcher matcher = pattern.matcher(line);
 
         while (matcher.find()) {
             String tokenValue = matcher.group();
             String tokenType;
-
-            if (tokenValue.matches(KEYWORD_PATTERN)) {
+             if (tokenValue.matches(PRE_PROCESSOR_PATTERN)) {
+                tokenType = "PRE_PROCESSOR_PATTERN";
+            } else if (tokenValue.matches(KEYWORD_PATTERN)) {
                 tokenType = "KEYWORD";
             } else if (tokenValue.matches(OPERATOR_PATTERN)) {
                 tokenType = "OPERATOR";
@@ -76,4 +82,5 @@ public class Lexical_Analyzer {
 
         return tokens;
     }
+
 }
--- Token.java
+++ Token.java
@@ -44,4 +44,5 @@ public class Token {
                 ", lineNumber=" + lineNumber +
                 '}';
     }
+
 }
