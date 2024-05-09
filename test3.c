facede1b8249bb7e58f448397179ae1a920e39fb
Yehia Sakr <yehiasakr4676@gmail.com>
2024-05-09 15:37:20 +0300

    Finish Parser, Merge all elements and add parseBody

--- test3.c
+++ test3.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
-int func (int a , int b){}
-
+int sum (int a , int b){
+    int value = a + b;
+    float s;
+    return value;
+}
 int main{
     float d;
-    int c;
-    int b;
-    int a = func (c,b);
+    int c = 5;
+    int b = 10;
+    int a = sum (c,b);
 
+
+    if(c>b){
+        for(int i=0;i<9;i++){
+            int g = 10 + 9;
+            switch(b){
+                case 1: d = 1.5;
+                        break;
+                case 2: c =6;
+                default: g = 4;
+            }
+        }
+        return;
+    }
 
     return 0;
 }
